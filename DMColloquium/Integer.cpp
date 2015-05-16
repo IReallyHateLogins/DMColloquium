845ef25b050e283f148ff674f81ed31aabe5c828
IReallyHateLogins <deathstingray@gmail.com>
2015-05-16 10:52:36 +0300

    Integer Summ With Switch
    ADD_NN_N with switch

--- DMColloquium/Integer.cpp
+++ DMColloquium/Integer.cpp
@@ -37,3 +37,31 @@ std::istream& operator>> (std::istream& str, Integer& integer) {
     
     return str;
 }
+
+
+Integer Integer::ADD_ZZ_Z(Integer n) {
+	if (this->POZ_Z_D == 0) return n;
+	if (n.POZ_Z_D == 0) return *this;
+	if (this->POZ_Z_D == 1 && n.POZ_Z_D == 1) 
+		return Integer(this->ADD_NN_N(n), true);
+	if (this->POZ_Z_D == -1 && n.POZ_Z_D == 1)
+	{
+		switch (this->COM_NN_D(n))
+		{
+			case 1: return Integer(n.SUB_NN_N(this->ABS_Z_N()), true);
+			case 0: return Integer();
+			case 2: return Integer(this->ABS_Z_N->SUB_NN_N(n), false);
+		}
+	}
+	if (this->POZ_Z_D == 1 && n.POZ_Z_D == -1)
+	{
+		switch (this->COM_NN_D(n))
+		{
+			case 1: return Integer(n.ABS_Z_N.SUB_NN_N(this), false);
+			case 0: return Integer();
+			case 2: return Integer(this->SUB_NN_N(n.ABS_Z_N()), false);
+		}
+	}
+	if (this->POZ_Z_D == -1 && n.POZ_Z_D == -1)
+		return Integer(this->ABS_Z_N->ADD_NN_N(n.ABS_Z_N), false);
+}
